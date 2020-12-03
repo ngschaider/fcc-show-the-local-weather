706b7359f159fbbaed5ea434a06dd511dc2a05f3
ngschaider <gschaiderniklas@gmail.com>
2020-12-03 10:34:56 +0100

    reworked js

--- script.js
+++ script.js
@@ -5,46 +5,40 @@
 var API_KEY = "d68f94524e5163da9293e572573751da";
 var TEMP_MODE = "CELSIUS";
 
-$(document).ready(function(){
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(gotPos);
-  }
-});
-
+(() => {
+  $("#request").click(() => {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(gotPos);
+    }
+  });
+})();
 
 function gotPos(position){
-  /*$("#longitude").html("Longitude: "+position.coords.longitude);
-  $("#latitude").html("Latitude: "+position.coords.latitude);*/
-  
-  $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&appid=b1b15e88fa797225412429c1c50c122a&apikey="+API_KEY, function(data){
-    
-    $("#city").html("<h3>"+data.name+", "+data.sys.country+": </h3>");
-    
-    var imgsrc = "";
-    if(data.weather[0].id == 800){ // CLEAR WEATHER
-      imgsrc = "https://ichef.bbci.co.uk/bbcle/images/width/live/p0/2g/j1/p02gj1yz.jpg/488";
-    }
-    else if(data.weather[0].id == 802){
-      imgsrc = "";
-    }
-    
-    var temp = convertKtoC(data.main.temp);
-    $("#temperature").html(temp + "C");
-    
-    $("#weather").html(data.weather[0].main + "<br><img src='"+imgsrc+"' style='height:100px; width:auto;' alt='Weather Symbol.'>");
-    $(".hidden").removeClass("hidden");
-  });
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+"&appid=b1b15e88fa797225412429c1c50c122a&apikey="+API_KEY;
+    $.getJSON(url, data => {
+      // set city and country
+      $("#city").html("<h3>"+data.name+", "+data.sys.country+": </h3>");
+
+      const iconUrl = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+      
+      const temp = convertKtoC(data.main.temp);
+      $("#temperature").html(temp + " °C");
+
+      $("#weather").html(data.weather[0].main + "<br><img src='" + iconUrl + "'>");
+      $(".container").removeClass("hidden");
+      $("#request").addClass("hidden");
+    });
 }
 
 $("#change-temp-mode").on("click", function(e){
   e.preventDefault();
   var temp = parseFloat( $("#temperature").html() );
   if(TEMP_MODE == "CELSIUS"){
-    $("#temperature").html(convertCtoF(temp)+"F");
+    $("#temperature").html(convertCtoF(temp)+" °F");
     TEMP_MODE = "FAHRENHEIT";
   }
   else{
-    $("#temperature").html(convertFtoC(temp)+"C");
+    $("#temperature").html(convertFtoC(temp)+" °C");
     TEMP_MODE = "CELSIUS";
   }
 });
